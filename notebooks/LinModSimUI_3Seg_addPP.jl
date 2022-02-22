a7fb5ad0e774a27939c0b0f548dfccaed7c23831
Jan Leppert <68964801+JanLeppert@users.noreply.github.com>
2022-02-22 18:09:28 +0100

    Update LinModSimUI_3Seg_addPP.jl

--- notebooks/LinModSimUI_3Seg_addPP.jl
+++ notebooks/LinModSimUI_3Seg_addPP.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.17.5
+# v0.17.7
 
 using Markdown
 using InteractiveUtils
@@ -22,6 +22,7 @@ begin
 	#using DataFrames, CSV, Interpolations, Plots, QuadGK, DifferentialEquations, ForwardDiff, Intervals
 	using Plots
 	using GasChromatographySystems, GasChromatographyTools
+	using HypertextLiteral
 	using PlutoUI
 	TableOfContents()
 end
@@ -52,25 +53,34 @@ md"""
 ## Definition of Parameters
 """
 
-# ╔═╡ 02a3b399-af72-464f-a121-bee782608fdf
-md"""
-### Options
+# ╔═╡ c5ec5bcb-4bcb-49c7-8ec3-8f4d1317fcbe
+function UI_Options()
+	PlutoUI.combine() do Child
+		@htl("""
+		<h3>Option settings</h3>
+		
+		mobile phase: $(
+			Child(Select(["H₂", "He", "N₂"]; default="He"))
+		) ODE algorithm: $(
+			Child(Select([OwrenZen3(), OwrenZen4(), OwrenZen5()]; default=OwrenZen5()))
+		) abstol: 1e $(
+			Child(NumberField(-10:1:-3; default=-8))
+		) reltol: 1e $(
+			Child(NumberField(-8:1:-2; default=-5))
+		) Tcontrol: $(
+			Child(Select(["inlet", "outlet"]; default="inlet"))
+		) ODE-system: $(
+			Child(CheckBox(default=true))
+		)
+		""")
+	end
+end
 
-mobile phase: $(@bind gas Select(["H₂", "He", "N₂"]; default="He"))
+# ╔═╡ 6d694987-d98f-4087-b0f3-515d4332f9af
+@bind opt_values confirm(UI_Options())
 
-ODE algorithmen: $(@bind alg Select(["OwrenZen3", "OwrenZen4", "OwrenZen5"]; default="OwrenZen5"))
-
-abs tol: 1e$(@bind abs NumberField(-8:1:-3; default=-6)) 
-
-rel tol: 1e$(@bind rel NumberField(-6:1:-1; default=-4))
-
-Tcontrol: $(@bind Tcontrol Select(["outlet", "inlet"]; default="inlet"))
-
-solve for ODE-system: $(@bind odesys CheckBox(default=true))
-"""
-
-# ╔═╡ 6137bf71-6663-49fe-baa5-a2618061a6db
-Option = GasChromatographySystems.Options(gas, alg, 10.0^abs, 10.0.^rel, Tcontrol, odesys);
+# ╔═╡ 75d2dcdc-25fc-404e-aa62-2b75170a7372
+Option = GasChromatographySystems.Options(opt_values[1], opt_values[2], 10.0^opt_values[3], 10.0.^opt_values[4], opt_values[5], opt_values[6])
 
 # ╔═╡ 531977ba-c3d7-49ed-ab64-05b04c8895a2
 md"""
@@ -82,135 +92,155 @@ $(LocalResource(string(pwd(),"/pic/Column.png")))
 $(LocalResource(string(pwd(),"/pic/PressurePoint.png")))
 $(LocalResource(string(pwd(),"/pic/Transferline.png")))
 $(LocalResource(string(pwd(),"/pic/PressurePoint.png")))
-
-stat. Phase:
-$(@bind sp1 Select(["", "Rxi17SilMS", "SLB5ms", "SPB50", "Wax", "FS5ms", "DB5ms", "Rxi5MS"]; default="Rxi17SilMS"))
-$(@bind sp2 Select(["", "Rxi17SilMS", "SLB5ms", "SPB50", "Wax", "FS5ms", "DB5ms", "Rxi5MS"]; default="Rxi17SilMS"))
-$(@bind sp3 Select(["", "Rxi17SilMS", "SLB5ms", "SPB50", "Wax", "FS5ms", "DB5ms", "Rxi5MS"]; default=""))
-
-L [m]:
-$(@bind L1 NumberField(0.10:0.01:10.00; default=0.16))
-$(@bind L2 NumberField(0.10:0.01:10.00; default=3.77))
-$(@bind L3 NumberField(0.10:0.01:10.00; default=0.395))
-
-d [mm]:
-$(@bind d1 NumberField(0.01:0.01:0.50; default=0.11))
-$(@bind d2 NumberField(0.01:0.01:0.50; default=0.11))
-$(@bind d3 NumberField(0.01:0.01:0.50; default=0.10))
-
-df [µm]:
-$(@bind df1 NumberField(0.00:0.01:0.50; default=0.1))
-$(@bind df2 NumberField(0.01:0.01:0.50; default=0.1))
-$(@bind df3 NumberField(0.00:0.01:0.50; default=0.00))
-
-T [°C]:
-$(@bind T1 NumberField(30.0:1.0:400.00; default=300.0))
-Temperature Program
-$(@bind T3 NumberField(30.0:1.0:400.00; default=300.0))
 """
 
-# ╔═╡ bcfd4677-099c-4be1-9da7-2a44a4ab0f74
-md"""
-### Temperature program
-
-gradient function:
-$(@bind gf Select(["linear", "exponential"]; default="exponential"))
-"""
-
-# ╔═╡ 91e7bc16-258f-44f7-9b32-b0ee9247045a
-begin
-	if gf=="linear"
-		md"""
-		time steps in s: $(@bind tsteps_str TextField((40,1); default="0 2 15 70 40 13 30"))
-	
-		temperature steps in °C: $(@bind Tsteps_str TextField((40,1); default="70 70 135 235 235 295 295"))
-		
-		ΔT [°C]: $(@bind ΔT_str TextField((40,1); default="0 0 0 0 0 0 0"))
-		
-		x₀ [m]: $(@bind x₀_str TextField((40,1); default="0 0 0 0 0 0 0"))
-		
-		L₀ [m]: $(L2)
-		
-		### Pressure program
-
-		time steps in s: $(@bind p_tsteps_str TextField((40,1); default="0 2 15 70 40 13 30"))
-
-		inlet pressure in kPa(g): $(@bind pin_str TextField((40,1); default="400 400 450 520 520 570 570"))
-
-		outlet pressure in kPa(a): $(@bind pout_str TextField((40,1); default="101.3 101.3 101.3 101.3")))
-		"""
-	elseif gf=="exponential"
-		md"""
-		time steps in s: $(@bind tsteps_str TextField((40,1); default="0 2 15 10 60 40 13 30"))
-	
-		temperature steps in °C: $(@bind Tsteps_str TextField((40,1); default="70 70 135 149 235 235 295 295"))
-		
-		ΔT [°C]: $(@bind ΔT_str TextField((40,1); default="30 30 52 46 30 30 52 55"))
-		
-		x₀ [m]: $(@bind x₀_str TextField((40,1); default="0 0 0 0 0 0 0 0"))
-		
-		L₀ [m]: $(L2)
-		
-		α: $(@bind α_str TextField((40,1); default="-4 -3 -4 -8 -11 -13 -13 -14"))
-		
-		### Pressure program
-
-		time steps in s: $(@bind p_tsteps_str TextField((40,1); default="0 2 15 70 40 13 30"))
-
-		inlet pressure in kPa(g): $(@bind pin_str TextField((40,1); default="300 300 350 420 420 470 470"))
-		
-		connector pressure in kPa(g): $(@bind pcon_str TextField((40,1); default="200 200 200 200 200 200 200"))
-
-		outlet pressure in kPa(a): $(@bind pout_str TextField((40,1); default="0 0 0 0 0 0 0"))
-		"""
+# ╔═╡ c45726d2-2920-4ac8-839f-ff876e8183ac
+function UI_System()
+	PlutoUI.combine() do Child
+		@htl("""
+		<h3>System settings</h3>
+		<table>
+			<tr>
+				<th>Parameter</th>
+				<th>Column 1</th>
+				<th>Column 2</th>
+				<th>Column 3</th>
+			</tr>
+			<tr>
+				<td>stat. phase</td>
+				<td><center>$(Child(Select(["", "Rxi17SilMS", "SLB5ms", "SPB50", "Wax", "FS5ms", "DB5ms", "Rxi5MS"]; default="DB5ms")))</center></td>
+				<td><center>$(Child(Select(["", "Rxi17SilMS", "SLB5ms", "SPB50", "Wax", "FS5ms", "DB5ms", "Rxi5MS"]; default="DB5ms")))</center></td>
+				<td><center>$(Child(Select(["", "Rxi17SilMS", "SLB5ms", "SPB50", "Wax", "FS5ms", "DB5ms", "Rxi5MS"]; default="DB5ms")))</center></td>
+			</tr>
+			<tr>
+				<td>L [m]</td>
+				<td><center>$(Child(NumberField(0.10:0.01:10.00; default=0.15)))</center></td>
+				<td><center>$(Child(NumberField(0.10:0.01:10.00; default=2.0)))</center></td>
+				<td><center>$(Child(NumberField(0.10:0.01:10.00; default=2.0)))</center></td>
+			</tr>
+			<tr>
+				<td>d [mm]</td>
+				<td><center>$(Child(NumberField(0.01:0.01:0.50; default=0.10)))</center></td>
+				<td><center>$(Child(NumberField(0.01:0.01:0.50; default=0.10)))</center></td>
+				<td><center>$(Child(NumberField(0.01:0.01:0.50; default=0.10)))</center></td>
+			</tr>
+			<tr>
+				<td>df [µm]</td>
+				<td><center>$(Child(NumberField(0.00:0.01:0.50; default=0.10)))</center></td>
+				<td><center>$(Child(NumberField(0.00:0.01:0.50; default=0.10)))</center></td>
+				<td><center>$(Child(NumberField(0.00:0.01:0.50; default=0.10)))</center></td>
+			</tr>
+			<tr>
+				<td>T [°C]</td>
+				<td><center>$(Child(NumberField(30.0:1.0:400.00; default=300.0)))</center></td>
+				<td><center>Temp. Prog.</center></td>
+				<td><center>$(Child(NumberField(30.0:1.0:400.00; default=300.0)))</center></td>
+			</tr>
+		</table>
+		""")
 	end
 end
 
+# ╔═╡ 10ad0aea-a5c1-4422-a632-e95a9cb1f7ef
+@bind sys_values confirm(UI_System())
+
+# ╔═╡ 6ae3059f-6cb3-417a-b6ab-78a3e44f74bd
+function UI_Program()
+	PlutoUI.combine() do Child
+		@htl("""
+		<h3>Temperature program</h3>
+		<table>
+			<tr>
+				<th>Parameter</th>
+				<th>Value</th>
+			</tr>
+			<tr>
+				<td>time steps [s]</td>
+				<td><center>$(Child(TextField((40,1); default="0 10 60 120")))</center></td>		
+			</tr>
+			<tr>
+				<td>temperature steps [°C]</td>
+				<td><center>$(Child(TextField((40,1); default="40 40 350 350")))</center></td>
+			</tr>
+			<tr>
+				<td>ΔT steps [°C]</td>
+				<td><center>$(Child(TextField((40,1); default="40 40 50 50")))</center></td>
+			</tr>
+			<tr>
+				<td>α steps [-]</td>
+				<td><center>$(Child(TextField((40,1); default="-5 -5 -5 -5")))</center></td>
+			</tr>
+		</table>
+		<h3>Pressure Program</h3>
+		<table>
+			<tr>
+				<th>Parameter</th>
+				<th>Value</th>
+			</tr>
+			<tr>
+				<td>time steps [s]</td>
+				<td><center>$(Child(TextField((40,1); default="0 10 60 120")))</center></td>
+			</tr>
+			<tr>
+				<td>inlet pressure [kPa(g)]</td>
+				<td><center>$(Child(TextField((40,1); default="400 400 500 500")))</center></td>
+			</tr>
+			<tr>
+				<td>connector pressure [kPa(g)]</td>
+				<td><center>$(Child(TextField((40,1); default="300 300 300 300")))</center></td>
+			</tr>
+			<tr>
+				<td>outlet pressure [kPa(g)]</td>
+				<td>$(Child(Select(["atmospheric", "vacuum"]; default="atmospheric")))</td>
+			</tr>
+		</table>
+		""")
+	end
+end
+
+# ╔═╡ c1b31764-00e3-45fc-91e9-eeeaa2d09f75
+@bind prog_values confirm(UI_Program())
+
 # ╔═╡ e42bf4b4-11c5-4db8-b018-52d355322e0d
 begin
-	if gf=="linear"
-		tsteps = parse.(Float64,split(tsteps_str))
-		Tsteps = parse.(Float64,split(Tsteps_str))
-		ΔT = parse.(Float64,split(ΔT_str))
-		x₀ = parse.(Float64,split(x₀_str))
-		L₀ = L2.*ones(length(ΔT))
-		α = zeros(length(ΔT))
-		a = [ΔT x₀ L₀ α]
-		grad_func(x) = GasChromatographySimulator.gradient(x, a; Tcontrol=Option.Tcontrol)
-	elseif gf=="exponential"
-		tsteps = parse.(Float64,split(tsteps_str))
-		Tsteps = parse.(Float64,split(Tsteps_str))
-		ΔT = parse.(Float64,split(ΔT_str))
-		x₀ = parse.(Float64,split(x₀_str))
-		L₀ = L2.*ones(length(ΔT))
-		α = parse.(Float64,split(α_str))
-		a = [ΔT x₀ L₀ α]
-		grad_func(x) = GasChromatographySimulator.gradient(x, a; Tcontrol=Option.Tcontrol)
-	end
+	L2 = sys_values[5]
+	tsteps = parse.(Float64,split(prog_values[1]))
+	Tsteps = parse.(Float64,split(prog_values[2]))
+	ΔT = parse.(Float64,split(prog_values[3]))
+	x₀ = zeros(length(ΔT))
+	L₀ = L2.*ones(length(ΔT))
+	α = parse.(Float64,split(prog_values[4]))
+	a = [ΔT x₀ L₀ α]
+	grad_func(x) = GasChromatographySimulator.gradient(x, a; Tcontrol=Option.Tcontrol)
+
 	TP = GasChromatographySystems.Temperature_Program(tsteps, Tsteps, grad_func, a)
-end;
+end
 
 # ╔═╡ f041e68e-d629-4f36-902c-7b77be067b1f
 begin
-	TL1 = GasChromatographySystems.Transferline(L1, d1*1e-3, df1*1e-6, sp1, T1)
-	a_d_2 = [d2*1e-3]
+	TL1 = GasChromatographySystems.Transferline(sys_values[4], sys_values[7]*1e-3, sys_values[10]*1e-6, sys_values[1], sys_values[13])
+	a_d_2 = [sys_values[8]*1e-3]
 	d_2(x) = GasChromatographySimulator.gradient(x, a_d_2)
-	a_df_2 = [df2*1e-6]
+	a_df_2 = [sys_values[11]*1e-6]
 	df_2(x) = GasChromatographySimulator.gradient(x, a_df_2)
-	GC  = GasChromatographySystems.Column(L2, d_2, a_d_2, df_2, a_df_2, sp2, TP)
-	TL2 = GasChromatographySystems.Transferline(L3, d3*1e-3, df3*1e-6, sp3, T3)
-end;
+	GC  = GasChromatographySystems.Column(sys_values[5], d_2, a_d_2, df_2, a_df_2, sys_values[2], TP)
+	TL2 = GasChromatographySystems.Transferline(sys_values[6], sys_values[9]*1e-3, sys_values[12]*1e-6, sys_values[3], sys_values[14])
+end
 
 # ╔═╡ 87ce0821-e017-4600-8e3e-b24759482a15
 begin
-	p_tsteps = parse.(Float64,split(p_tsteps_str))
-	pin_steps = parse.(Float64,split(pin_str))
-	pcon_steps = parse.(Float64,split(pcon_str))
-	pout_steps = parse.(Float64,split(pout_str))
+	p_tsteps = parse.(Float64,split(prog_values[5]))
+	pin_steps = parse.(Float64,split(prog_values[6]))
+	pcon_steps = parse.(Float64,split(prog_values[7]))
+	if prog_values[8] == "atmospheric"
+		pout_steps = 101300.0.*ones(length(p_tsteps))
+	elseif prog_values[8] == "vacuum"
+		pout_steps = zeros(length(p_tsteps))
+	end
 	PPin = GasChromatographySystems.Pressure_Point(p_tsteps, pin_steps.*1000.0.+101300.0)
 	PPcon = GasChromatographySystems.Pressure_Point(p_tsteps, pcon_steps.*1000.0.+101300.0)
-	PPout = GasChromatographySystems.Pressure_Point(p_tsteps, pout_steps.*1000.0)
-end;
+	PPout = GasChromatographySystems.Pressure_Point(p_tsteps, pout_steps)
+end
 
 # ╔═╡ 08142bda-450f-42fe-aece-6f0bd5e28717
 GCsys = PPin, TL1, GC, PPcon, TL2, PPout
@@ -454,7 +484,7 @@ GasChromatographySystems.test_of_GCsys(GCsys)
 md"""
 ### Solutes
 
-Selection of solutes: $(@bind selection Select(["all", "Alkanes", "PAH", "manually"]; default="PAH"))
+Selection of solutes: $(@bind selection Select(["all", "Alkanes", "PAH", "manually"]; default="Alkanes"))
 """
 
 # ╔═╡ fd69382e-5e7e-4409-9754-f9177d8f35c4
@@ -601,9 +631,9 @@ Simulate: $(@bind go CheckBox(default=false))
 
 # ╔═╡ f382e2e5-38ae-4558-99ff-74213b488c1b
 begin
-	if go==true
-		par, sol = GasChromatographySystems.linear_GC_system_simulation(GCsys, Option, solutes, db)
-	end
+	#if go==true
+		par, pl, sol = GasChromatographySystems.linear_GC_system_simulation(GCsys, Option, solutes, db)
+	#end
 end;
 
 # ╔═╡ af016139-05bc-4623-b72e-1c4726024c45
@@ -613,9 +643,9 @@ md"""
 
 # ╔═╡ 16ef205f-5129-4bdc-8bb7-84b1282c18ef
 begin
-	pl1 = peaklist(sol[1], par[1])
-	pl2 = peaklist(sol[2], par[2])
-	pl3 = peaklist(sol[end], par[end])
+	pl1 = pl[1]
+	pl2 = pl[2]
+	pl3 = pl[3]
 	peaklist_combi = DataFrame(Name=pl3.Name, tR_D=pl3.tR, τR_D=pl3.τR, TR_C=pl2.TR, σR_D=pl3.σR, uR_D=pl3.uR, kR_C=pl2.kR, Res_D=pl3.Res)
 	md"""
 	### Peaklist
@@ -649,6 +679,14 @@ begin
 	"""
 end
 
+# ╔═╡ 3f52ec2f-cac1-4c60-b589-f52f4b388273
+begin
+	p_Res = plot(pl1.Name, pl1.Res, ylabel="resolution", label="TL1")
+	plot!(p_Res, pl2.Name, pl2.Res, label="GC1")
+	plot!(p_Res, pl3.Name, pl3.Res, label="TL2")
+	p_Res
+end
+
 # ╔═╡ 59e8e493-4d40-42cd-bc2e-c3dc415aac45
 begin
 	export_str = string("Options: \n", option_str(Option), "GC-system settings: \n", df_to_string(settings(GCsys)), "\n Peaklist: \n", pl_to_string(peaklist_combi))
@@ -674,35 +712,39 @@ md"""
 # ╔═╡ Cell order:
 # ╠═1cce655a-1ba6-11ec-1bf4-edfded0254c8
 # ╟─ed577478-23b5-4c76-95ef-8de32307225e
-# ╠═ed14d2ca-9a09-41fa-a67a-8190161646b6
+# ╟─ed14d2ca-9a09-41fa-a67a-8190161646b6
 # ╟─f5075b30-4b3f-46b8-bb9a-6c8c01d6af9f
 # ╟─f82f4dfc-4bb5-47a0-9a7a-f4faf86360f7
-# ╟─02a3b399-af72-464f-a121-bee782608fdf
-# ╠═6137bf71-6663-49fe-baa5-a2618061a6db
-# ╠═531977ba-c3d7-49ed-ab64-05b04c8895a2
-# ╠═f041e68e-d629-4f36-902c-7b77be067b1f
-# ╟─bcfd4677-099c-4be1-9da7-2a44a4ab0f74
-# ╠═91e7bc16-258f-44f7-9b32-b0ee9247045a
-# ╠═e42bf4b4-11c5-4db8-b018-52d355322e0d
+# ╟─6d694987-d98f-4087-b0f3-515d4332f9af
+# ╟─75d2dcdc-25fc-404e-aa62-2b75170a7372
+# ╟─c5ec5bcb-4bcb-49c7-8ec3-8f4d1317fcbe
+# ╟─531977ba-c3d7-49ed-ab64-05b04c8895a2
+# ╟─10ad0aea-a5c1-4422-a632-e95a9cb1f7ef
+# ╟─f041e68e-d629-4f36-902c-7b77be067b1f
+# ╟─c45726d2-2920-4ac8-839f-ff876e8183ac
+# ╟─c1b31764-00e3-45fc-91e9-eeeaa2d09f75
+# ╟─6ae3059f-6cb3-417a-b6ab-78a3e44f74bd
+# ╟─e42bf4b4-11c5-4db8-b018-52d355322e0d
 # ╟─87ce0821-e017-4600-8e3e-b24759482a15
-# ╠═08142bda-450f-42fe-aece-6f0bd5e28717
-# ╠═bb5c36f1-5b47-4055-b10c-0b1873c4a5c8
-# ╠═0cac5ac9-31f7-4bf0-b71b-b8e691e8d5ea
-# ╠═fd69382e-5e7e-4409-9754-f9177d8f35c4
+# ╟─08142bda-450f-42fe-aece-6f0bd5e28717
+# ╟─bb5c36f1-5b47-4055-b10c-0b1873c4a5c8
+# ╟─0cac5ac9-31f7-4bf0-b71b-b8e691e8d5ea
+# ╟─fd69382e-5e7e-4409-9754-f9177d8f35c4
 # ╟─6fef3cba-8917-480a-bdf2-a3ae472c02e3
 # ╟─0542086b-0f06-40e5-8ea6-9e788a4ded7f
-# ╠═f781ca29-9d94-4a82-9ab8-41046b3453dc
-# ╠═6114231c-3f8a-4e87-8a19-629e4f03e900
+# ╟─f781ca29-9d94-4a82-9ab8-41046b3453dc
+# ╟─6114231c-3f8a-4e87-8a19-629e4f03e900
 # ╟─e2b9d8bc-038b-4d09-8558-27bf660c9882
 # ╟─b4b3ce39-9d6b-49d2-9b3e-2852d0401c9a
 # ╟─adaf8dfb-7c62-476a-9bb9-fb34ff949abf
 # ╟─da3f7878-99d7-41fd-9294-57664cfd1c10
-# ╠═a4aadd32-84e8-4d5d-a54d-877da644e2bc
+# ╟─a4aadd32-84e8-4d5d-a54d-877da644e2bc
 # ╟─bbe1f0ae-3675-4b3b-b012-c25304c38689
-# ╠═f382e2e5-38ae-4558-99ff-74213b488c1b
+# ╟─f382e2e5-38ae-4558-99ff-74213b488c1b
 # ╟─af016139-05bc-4623-b72e-1c4726024c45
 # ╠═16ef205f-5129-4bdc-8bb7-84b1282c18ef
 # ╟─7b0ede51-4a76-4360-b09d-23b002ae6652
+# ╠═3f52ec2f-cac1-4c60-b589-f52f4b388273
 # ╟─59e8e493-4d40-42cd-bc2e-c3dc415aac45
 # ╟─15ee4fd0-7e13-4e3c-ba54-9e6601279a68
 # ╠═60738259-20ea-4c69-be9f-aadddbc05b8f

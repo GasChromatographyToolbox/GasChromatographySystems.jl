738a345728af6a8b4401610100212d8afe65b31c
Jan Leppert <68964801+JanLeppert@users.noreply.github.com>
2023-05-16 17:58:33 +0200

    Update GCxGC_presentation.jl

--- notebooks/GCxGC_presentation.jl
+++ notebooks/GCxGC_presentation.jl
@@ -21,15 +21,6 @@ begin
 	TableOfContents()
 end
 
-# ╔═╡ fc787595-9163-410f-82f4-2252f5ebaf63
-using Interpolations
-
-# ╔═╡ 60a07e1e-f85d-45af-908b-b9ae3adc6220
-using BenchmarkTools
-
-# ╔═╡ 2d2882e1-5980-45a7-9891-9b328de9fa3d
-using Profile
-
 # ╔═╡ 139bfe64-17ac-455d-84a2-09c9564156cd
 md"""
 # Simple GCxGC system
@@ -279,81 +270,38 @@ md"""
 paths = GasChromatographySystems.all_paths(sys.g, 1)[2]
 
 # ╔═╡ f106b5fd-e17a-4876-8562-e82670a1ab25
-sim_res = GasChromatographySystems.simulate_along_paths(sys, paths, db, selected_solutes, par)
+sim_res = GasChromatographySystems.simulate_along_paths(sys, paths, par)
 
 # ╔═╡ d8c30c13-7209-43ea-93c8-ac0310b0a8e5
 begin
 	par_ = GasChromatographySystems.graph_to_parameters(sys, db, selected_solutes)
 	paths_ = GasChromatographySystems.all_paths(sys.g, 1)[2]
-	sim_res_ = GasChromatographySystems.simulate_along_paths(sys, paths_, db, selected_solutes, par_)
+	sim_res_ = GasChromatographySystems.simulate_along_paths(sys, paths_, par_)
 end
 
 # ╔═╡ 7dac264f-2e91-44bd-b96d-207f072cba9c
-sim_res_foc = GasChromatographySystems.simulate_along_paths(sys, paths, db, selected_solutes, par; refocus=[true, true])
+sim_res_foc = GasChromatographySystems.simulate_along_paths(sys, paths, par; refocus=[true, true])
 
 # ╔═╡ 5373eaae-4372-4561-83cf-b62664bf28af
 sim_res[2][1][1]
 
-# ╔═╡ 0bc7a04d-7534-4e0b-993c-a6ca65f8d363
-pl = GasChromatographySystems.peaklist_GCxGC(sim_res[2][1][1], sim_res[2][1][2])
-
-# ╔═╡ 5429291b-8efd-4ebb-9d4d-1be143073482
-pl_foc = GasChromatographySystems.peaklist_GCxGC(sim_res_foc[2][1][1], sim_res_foc[2][1][2])
-
 # ╔═╡ c5888be4-c641-4871-81a5-11d01ace12ed
 #savefig(p_GCxGC_cats, "chrom_GCxGC_10.svg")
 
-# ╔═╡ 127069e6-6da1-4742-88b4-a0d398a88b24
-function plot_GCxGC(pl_GCxGC, sys; categories = String[])
-	t1end = sum(GasChromatographySystems.common_timesteps(sys))
-	t2end = ceil(maximum(pl_GCxGC.tR2.-pl_GCxGC.tR1))
-	p_gcxgc = Plots.scatter(pl_GCxGC.tR1, pl_GCxGC.tR2.-pl_GCxGC.tR1, xlims=(0.0,t1end), ylims=(0.0, t2end), label="", xlabel=string(sys.modules[1].stationary_phase, ", t¹ in s"), ylabel=string(sys.modules[2].stationary_phase, ", t² in s"))
-	if !isempty(categories)
-		for i=1:length(categories) 
-			pl_f = filter([:Cat] => x -> categories[i] in x, pl_GCxGC)
-			Plots.scatter!(p_gcxgc, pl_f.tR1, pl_f.tR2.-pl_f.tR1, label=categories[i], legend=:bottomright)
-		end
-	end
-	return p_gcxgc
-end
-
-# ╔═╡ 3986a24a-689c-48b8-8e78-ec5d3842d0d7
-plot_GCxGC(pl, sys)
-
-# ╔═╡ 938d8677-7898-43c8-b07c-6e9d7f3c7e2e
-begin
-	gr()
-	p_GCxGC_cats = plot_GCxGC(pl, sys; categories = ["saturated FAME", "unsaturated FAME", "aromatic", "PCB", "alkanes", "alcohols", "alkanones", "phenones"])
-	p_GCxGC_cats
-end
-
 # ╔═╡ 987efbb5-8a48-4c83-9a20-50e5fbd0a16d
-function plot_GCxGC_contour_(pl_GCxGC; split_ratio=ones(length(pl_GCxGC.Name)))
+function plot_GCxGC_contour_(pl_GCxGC; split_ratio=ones(length(pl_GCxGC.Name)), mod_τ1=1.0)
 	t¹ = 0.9*minimum(pl_GCxGC.tR1):(1.1*maximum(pl_GCxGC.tR1)-0.9*minimum(pl_GCxGC.tR1))/1000:1.1*maximum(pl_GCxGC.tR1)
 	t² = 0.9*minimum(pl_GCxGC.tR2.-pl_GCxGC.tR1):(1.1*maximum(pl_GCxGC.tR2.-pl_GCxGC.tR1)-0.9*minimum(pl_GCxGC.tR2.-pl_GCxGC.tR1))/1000:1.1*maximum(pl_GCxGC.tR2.-pl_GCxGC.tR1)
 	chrom2D = Array{Float64}(undef, length(t¹), length(t²), length(pl_GCxGC.Name))
 	for j=1:length(t²)
 		for i=1:length(t¹)
 			for k=1:length(pl_GCxGC.Name)
-				chrom2D[i,j,k] = split_ratio[k]*exp(-((t¹[i]-pl_GCxGC.tR1[k])^2/(2*pl_GCxGC.τR1[k]^2)+(t²[j]-(pl_GCxGC.tR2[k]-pl_GCxGC.tR1[k]))^2/(2*pl_GCxGC.τR2[k]^2)))
+				chrom2D[i,j,k] = split_ratio[k]*exp(-((t¹[i]-pl_GCxGC.tR1[k])^2/(2*(mod_τ1*pl_GCxGC.τR1[k])^2)+(t²[j]-(pl_GCxGC.tR2[k]-pl_GCxGC.tR1[k]))^2/(2*pl_GCxGC.τR2[k]^2)))
 			end
 		end
 	end
 	#p_2D = Plots.contourf(t¹, t², sum(chrom2D, dims=3)[:,:,1]', fill=true, legend=false, xlabel="tR1 in s", ylabel="tR2 in s", c=:turbo, levels=40)
 	return t¹, t², sum(chrom2D, dims=3)[:,:,1]'
-end
-
-# ╔═╡ 939e3a45-03cd-4e07-a672-10c32cf2637e
-t¹, t², chrom = plot_GCxGC_contour_(pl_foc)
-
-# ╔═╡ 7100eeed-5604-4d81-855e-b08cd40135a6
-Plots.contour(t¹, t², chrom, legend=false, xlabel="tR1 in s", ylabel="tR2 in s", c=:turbo, levels=100, xlims=(0.0, 1700.0), ylims=(0.0, 5.0), grid=false)
-
-# ╔═╡ 00fbcd7c-71a1-43f7-b358-abfde851a237
-begin
-	plotly()
-	p_GCxGC = Plots.heatmap(t¹, t², chrom.^(1//3), legend=false, xlabel="tR1 in s", ylabel="tR2 in s", c=:jet1, dpi=500)#, xlims=(0.0, 1700.0), ylims=(0.0, 5.0), grid=false)
-	gui()
 end
 
 # ╔═╡ fba31c65-0465-458a-8fa5-7df1abdf179c
@@ -385,23 +333,7 @@ par_1a = GasChromatographySystems.graph_to_parameters(sys_1a, db, selected_solut
 paths_1a = GasChromatographySystems.all_paths(sys_1a.g, 1)[2]
 
 # ╔═╡ dfc605b8-bc23-4456-8dbe-6cb83f340785
-sim_res_1a = GasChromatographySystems.simulate_along_paths(sys_1a, paths_1a, db, selected_solutes, par_1a; refocus=[true, true])
-
-# ╔═╡ 6a1e1c53-93da-4b2d-a65d-dd13fd86c174
-pl_1a = GasChromatographySystems.peaklist_GCxGC(sim_res_1a[2][1][1], sim_res_1a[2][1][2])
-
-# ╔═╡ 9976debc-cbbf-462a-a1f6-b4f9aa468cd2
-plot_GCxGC(pl_1a, sys_1a)
-
-# ╔═╡ 1143dfde-bc45-4c2f-b90c-be6afe7bb35a
-#t¹_1a, t²_1a, chrom_1a = plot_GCxGC_contour_(pl_1a)
-
-# ╔═╡ 5771318a-215a-440d-bc13-d71e042a5c03
-begin
-#	plotly()
-#	p_GCxGC_1a = Plots.heatmap(t¹_1a, t²_1a, chrom_1a.^(1//3), legend=false, xlabel="tR1 in s", ylabel="tR2 in s", c=:jet1, dpi=500)#, xlims=(0.0, 1700.0), ylims=(0.0, 5.0), grid=false)
-	#gui()
-end
+sim_res_1a = GasChromatographySystems.simulate_along_paths(sys_1a, paths_1a, par_1a; refocus=[true, true])
 
 # ╔═╡ e7956916-dccb-46c1-a30b-ef77650e54ce
 md"""
@@ -415,13 +347,7 @@ par_rev = GasChromatographySystems.graph_to_parameters(sys_rev, db, selected_sol
 paths_rev = GasChromatographySystems.all_paths(sys_rev.g, 1)[2]
 
 # ╔═╡ aa0d4fda-3f1b-468c-9568-acadf61b8b95
-sim_res_rev = GasChromatographySystems.simulate_along_paths(sys_rev, paths_rev, db, selected_solutes, par_rev)
-
-# ╔═╡ 397a913b-fa6c-4eba-b8b7-7571256a001b
-pl_rev = GasChromatographySystems.peaklist_GCxGC(sim_res_rev[2][1][1], sim_res_rev[2][1][2])
-
-# ╔═╡ c3f16558-43b8-4822-98af-daf8e06cd1df
-p_GCxGC_cats_rev = plot_GCxGC(pl_rev, sys_rev; categories = ["saturated FAME", "unsaturated FAME", "aromatic", "PCB", "alkanes", "alcohols", "alkanones", "phenones"])
+sim_res_rev = GasChromatographySystems.simulate_along_paths(sys_rev, paths_rev, par_rev)
 
 # ╔═╡ 834739e9-fa7f-43f0-beac-ae5790575255
 #savefig(p_GCxGC_cats_rev, "chrom_GCxGC_8_rev.svg")
@@ -430,89 +356,6 @@ p_GCxGC_cats_rev = plot_GCxGC(pl_rev, sys_rev; categories = ["saturated FAME", "
 md"""
 ### 1c. Flow modulator
 """
-
-# ╔═╡ d298a226-f5df-4636-9b41-2a302858592f
-# graph_to_parameters has to be modified for defined Flows
-# interpolate_pressure_functions() seems to be to slow
-#par_1c = GasChromatographySystems.graph_to_parameters(sys_1c, db, selected_solutes)
-
-# ╔═╡ 3e34982e-f791-4d96-9dde-27ee29720114
-tsteps_ = GasChromatographySystems.common_timesteps(sys_1c)
-
-# ╔═╡ d89e4c44-6335-40ec-a158-ce1940a57cd5
-tend_ = sum(tsteps_)
-
-# ╔═╡ 47901238-c0ff-45f3-a0c7-f0bd60f10162
-all(degree(sys_1c.g).<3)
-
-# ╔═╡ f89db6b4-d805-4a1e-b9e6-43542c29e204
-trange_ = 0:1:tend_
-
-# ╔═╡ d3a24890-989e-4832-a98a-ce60e5b5bdf0
-length(trange_)
-
-# ╔═╡ bc36251b-b48d-42af-8c74-ae70744a0838
-p_func_ = GasChromatographySystems.pressure_functions(sys_1c)
-
-# ╔═╡ 8196a315-eeb8-457b-8f2c-5b4591521f5f
-p_itp_ = Array{Any}(undef, length(p_func_))
-
-# ╔═╡ b4fd1811-ee6d-4f8d-87d7-2f13c328060a
-all(isnan.(sys_1c.pressurepoints[1].pressure_steps))
-
-# ╔═╡ eed9dc74-6b7c-4fd1-b6e0-0f28a213ff26
-#p_itp_[1] = LinearInterpolation((trange_, ), p_func_[1].(trange_), extrapolation_bc=Flat())
-
-# ╔═╡ be54942b-0bec-49cf-9687-0a70143a63bd
-p_itp_test = linear_interpolation(collect(0.0:60.0:3540.0), p_func_[1].(0.0:60.0:3540.0), extraploation_bc=Interpolations.Flat())
-
-# ╔═╡ 0e3395a1-4137-4264-8309-49c0c7e66e42
-tt = 0.0
-
-# ╔═╡ 7cd1fb34-8f79-4d0d-801b-c2b6883085f2
-@benchmark p_func_[1]($tt)
-
-# ╔═╡ 086173f9-a16b-4201-9b1e-439ae9e08dc8
-@benchmark p_func_[2]($tt)
-
-# ╔═╡ f2c3a527-bc28-4f58-aedb-74101c383293
-@benchmark p_func_[3]($tt)
-
-# ╔═╡ 7479fd7b-c81c-475a-9dbb-fdaea5ab8005
-@benchmark p_func_[4]($tt)
-
-# ╔═╡ c1e71e46-7b55-424b-b242-5609ddbd9a10
-begin
-	@profile p_func_[1](tt)
-	Profile.print()
-end
-
-# ╔═╡ f8f6bf15-f5a6-4738-8bbd-d7adc9a214ed
-itp = linear_interpolation(collect(0.0:60.0:3540.0), p_func_[1].(0.0:60.0:3540.0))
-
-# ╔═╡ b142fbc0-baca-4205-83d5-ea85c55baf7c
-itp(0)
-
-# ╔═╡ b9727123-582b-4f28-a073-c004e97d8b24
-itp(3541)
-
-# ╔═╡ 1d3d3ed9-4127-4fcf-822a-43f38c5632c7
-knots = 0.0:60.0:3540.0
-
-# ╔═╡ 1d94af98-b90b-4a57-af2d-eaf0aefa8fbd
-A = p_func_[1].(knots) # the evaluation of the function takes a long time
-
-# ╔═╡ 37f7be98-67f0-43e2-b8b1-e2d48f6f8e53
-iitp = extrapolate(scale(interpolate(A, BSpline(Linear())), knots), Interpolations.Flat())
-
-# ╔═╡ 83470d02-8fcd-45d5-acda-3f28da0c547b
-extrapolate(scale(interpolate(p_func_[1].(knots), BSpline(Linear())), knots), Interpolations.Flat())
-
-# ╔═╡ dc712259-89aa-4138-a7b9-042956c89a33
-iitp(30000)
-
-# ╔═╡ c15d42ef-3b56-4093-b84a-0df13e46132f
-#GasChromatographySystems.interpolate_pressure_functions(sys_1c)
 
 # ╔═╡ fa011932-61e2-4377-bbf9-eb0bcd8c1b22
 par_1c = GasChromatographySystems.graph_to_parameters(sys_1c, db, selected_solutes; interp=true, dt=10) # using the calculated functions for the pressures
@@ -523,23 +366,7 @@ par_1c = GasChromatographySystems.graph_to_parameters(sys_1c, db, selected_solut
 paths_1c = GasChromatographySystems.all_paths(sys_1c.g, 1)[2]
 
 # ╔═╡ fcba3e01-9464-4f95-8144-d8d1671b265e
-sim_res_1c = GasChromatographySystems.simulate_along_paths(sys_1c, paths_1c, db, selected_solutes, par_1c)
-
-# ╔═╡ 7bcca943-1b5a-4eb5-a419-30a1959c53aa
-pl_1c = GasChromatographySystems.peaklist_GCxGC(sim_res_1c[2][1][1], sim_res_1c[2][1][2])
-
-# ╔═╡ ee516d3e-d60b-49c6-aa2f-eb7cb3a9acd1
-plot_GCxGC(pl_1c, sys_1c)
-
-# ╔═╡ ec9dcf65-93ca-4299-a5a7-578aca60e906
-t¹_1c, t²_1c, chrom_1c = plot_GCxGC_contour_(pl_1c)
-
-# ╔═╡ a6b8b92d-e7a7-4b1a-9a12-1db83b530e13
-begin
-	plotly()
-	p_GCxGC_1c = Plots.heatmap(t¹_1c, t²_1c, chrom_1c.^(1//3), legend=false, xlabel="tR1 in s", ylabel="tR2 in s", c=:jet1, dpi=500)#, xlims=(0.0, 1700.0), ylims=(0.0, 5.0), grid=false)
-	#gui()
-end
+sim_res_1c = GasChromatographySystems.simulate_along_paths(sys_1c, paths_1c, par_1c)
 
 # ╔═╡ 679140af-7598-41fe-aad1-90c50e503218
 md"""
@@ -553,13 +380,7 @@ par_offset = GasChromatographySystems.graph_to_parameters(sys_offset, db, select
 paths_offset = GasChromatographySystems.all_paths(sys_offset.g, 1)[2]
 
 # ╔═╡ 097f05ad-8723-4693-bfba-c350cd478f54
-sim_res_offset = GasChromatographySystems.simulate_along_paths(sys_offset, paths_offset, db, selected_solutes, par_offset)
-
-# ╔═╡ 417fa98b-38d1-434a-a44c-2948d71bec42
-pl_offset = peaklist_GCxGC(sim_res_offset[2][1][1], sim_res_offset[2][1][2])
-
-# ╔═╡ 28fbe11a-4132-41e1-8ea8-3b65749f41b1
-p_GCxGC_cats_offset = plot_GCxGC(pl_offset, sys_offset; categories = ["saturated FAME", "unsaturated FAME", "aromatic", "PCB", "alkanes", "alcohols", "alkanones", "phenones"])
+sim_res_offset = GasChromatographySystems.simulate_along_paths(sys_offset, paths_offset, par_offset)
 
 # ╔═╡ 8d14ae41-a062-4d89-8e5a-e78b62cc0564
 #savefig(p_GCxGC_cats_offset, "chrom_GCxGC_8_offset.svg")
@@ -568,6 +389,115 @@ p_GCxGC_cats_offset = plot_GCxGC(pl_offset, sys_offset; categories = ["saturated
 md"""
 # End
 """
+
+# ╔═╡ 127069e6-6da1-4742-88b4-a0d398a88b24
+function plot_GCxGC(pl_GCxGC, sys; categories = String[])
+	t1end = sum(GasChromatographySystems.common_timesteps(sys))
+	t2end = ceil(maximum(pl_GCxGC.tR2.-pl_GCxGC.tR1))
+	p_gcxgc = Plots.scatter(pl_GCxGC.tR1, pl_GCxGC.tR2.-pl_GCxGC.tR1, xlims=(0.0,t1end), ylims=(0.0, t2end), label="", xlabel=string(sys.modules[1].stationary_phase, ", t¹ in s"), ylabel=string(sys.modules[2].stationary_phase, ", t² in s"))
+	if !isempty(categories)
+		for i=1:length(categories) 
+			pl_f = filter([:Cat] => x -> occursin(categories[i], x), pl_GCxGC)
+			Plots.scatter!(p_gcxgc, pl_f.tR1, pl_f.tR2.-pl_f.tR1, label=categories[i], legend=:bottomright)
+		end
+	end
+	return p_gcxgc
+end
+
+# ╔═╡ 417fa98b-38d1-434a-a44c-2948d71bec42
+function peaklist_GCxGC_with_categories(pl_D1, pl_D2, db)
+	pl = GasChromatographySystems.peaklist_GCxGC(pl_D1, pl_D2)
+	cat = Array{String}(undef, length(pl.Name))
+	for i=1:length(pl.Name)
+		ii = findfirst(pl.Name[i].==db.Name)
+		cat[i] = ""
+		for j=1:length(findall(occursin.("Cat", names(db))))
+			if !ismissing(db[ii, Symbol("Cat_$(j)")])
+				if j==1
+					cat[i] = cat[i]*db[ii, Symbol("Cat_$(j)")]
+				else
+					cat[i] = cat[i]*", "*db[ii, Symbol("Cat_$(j)")]
+				end
+			end
+		end
+	end
+	pl[!, :Cat] = cat
+	return pl
+end
+
+# ╔═╡ 0bc7a04d-7534-4e0b-993c-a6ca65f8d363
+pl = peaklist_GCxGC_with_categories(sim_res[2][1][1], sim_res[2][1][2], db)
+
+# ╔═╡ 3986a24a-689c-48b8-8e78-ec5d3842d0d7
+plot_GCxGC(pl, sys)
+
+# ╔═╡ 938d8677-7898-43c8-b07c-6e9d7f3c7e2e
+begin
+	gr()
+	p_GCxGC_cats = plot_GCxGC(pl, sys; categories = ["saturated FAME", "unsaturated FAME", "aromatic", "PCB", "alkanes", "alcohols", "alkanones", "phenones"])
+	p_GCxGC_cats
+end
+
+# ╔═╡ 5429291b-8efd-4ebb-9d4d-1be143073482
+pl_foc =peaklist_GCxGC_with_categories(sim_res_foc[2][1][1], sim_res_foc[2][1][2], db)
+
+# ╔═╡ 939e3a45-03cd-4e07-a672-10c32cf2637e
+t¹, t², chrom = plot_GCxGC_contour_(pl_foc)
+
+# ╔═╡ 7100eeed-5604-4d81-855e-b08cd40135a6
+Plots.contour(t¹, t², chrom, legend=false, xlabel="tR1 in s", ylabel="tR2 in s", c=:turbo, levels=100, xlims=(0.0, 1700.0), ylims=(0.0, 5.0), grid=false)
+
+# ╔═╡ 00fbcd7c-71a1-43f7-b358-abfde851a237
+begin
+	plotly()
+	p_GCxGC = Plots.heatmap(t¹, t², chrom.^(1//3), legend=false, xlabel="tR1 in s", ylabel="tR2 in s", c=:jet1, dpi=500)#, xlims=(0.0, 1700.0), ylims=(0.0, 5.0), grid=false)
+	#gui()
+end
+
+# ╔═╡ 6a1e1c53-93da-4b2d-a65d-dd13fd86c174
+pl_1a = peaklist_GCxGC_with_categories(sim_res_1a[2][1][1], sim_res_1a[2][1][2], db)
+
+# ╔═╡ 9976debc-cbbf-462a-a1f6-b4f9aa468cd2
+plot_GCxGC(pl_1a, sys_1a; categories = ["saturated FAME", "unsaturated FAME", "aromatic", "PCB", "alkanes", "alcohols", "alkanones", "phenones"])
+
+# ╔═╡ 1143dfde-bc45-4c2f-b90c-be6afe7bb35a
+t¹_1a, t²_1a, chrom_1a = plot_GCxGC_contour_(pl_1a; mod_τ1=1.5)
+
+# ╔═╡ 5771318a-215a-440d-bc13-d71e042a5c03
+begin
+	plotly()
+	#gr()
+	p_GCxGC_1a = Plots.heatmap(t¹_1a, t²_1a, chrom_1a.^(1//3), legend=false, xlabel="tR1 in s", ylabel="tR2 in s", c=:jet1, dpi=500)#, xlims=(0.0, 1700.0), ylims=(0.0, 5.0), grid=false)
+	#gui()
+end
+
+# ╔═╡ 397a913b-fa6c-4eba-b8b7-7571256a001b
+pl_rev = peaklist_GCxGC_with_categories(sim_res_rev[2][1][1], sim_res_rev[2][1][2], db)
+
+# ╔═╡ c3f16558-43b8-4822-98af-daf8e06cd1df
+p_GCxGC_cats_rev = plot_GCxGC(pl_rev, sys_rev; categories = ["saturated FAME", "unsaturated FAME", "aromatic", "PCB", "alkanes", "alcohols", "alkanones", "phenones"])
+
+# ╔═╡ 7bcca943-1b5a-4eb5-a419-30a1959c53aa
+pl_1c = peaklist_GCxGC_with_categories(sim_res_1c[2][1][1], sim_res_1c[2][1][2], db)
+
+# ╔═╡ ee516d3e-d60b-49c6-aa2f-eb7cb3a9acd1
+plot_GCxGC(pl_1c, sys_1c; categories = ["saturated FAME", "unsaturated FAME", "aromatic", "PCB", "alkanes", "alcohols", "alkanones", "phenones"])
+
+# ╔═╡ ec9dcf65-93ca-4299-a5a7-578aca60e906
+t¹_1c, t²_1c, chrom_1c = plot_GCxGC_contour_(pl_1c)
+
+# ╔═╡ a6b8b92d-e7a7-4b1a-9a12-1db83b530e13
+begin
+	plotly()
+	p_GCxGC_1c = Plots.heatmap(t¹_1c, t²_1c, chrom_1c.^(1//3), legend=false, xlabel="tR1 in s", ylabel="tR2 in s", c=:jet1, dpi=500)#, xlims=(0.0, 1700.0), ylims=(0.0, 5.0), grid=false)
+	#gui()
+end
+
+# ╔═╡ a0d12beb-a1e8-4f32-902d-233308f0a783
+pl_offset = peaklist_GCxGC_with_categories(sim_res_offset[2][1][1], sim_res_offset[2][1][2], db)
+
+# ╔═╡ 28fbe11a-4132-41e1-8ea8-3b65749f41b1
+p_GCxGC_cats_offset = plot_GCxGC(pl_offset, sys_offset; categories = ["saturated FAME", "unsaturated FAME", "aromatic", "PCB", "alkanes", "alcohols", "alkanones", "phenones"])
 
 # ╔═╡ Cell order:
 # ╠═e9d64f0e-95b7-11ed-2fee-ab579cd02ebc
@@ -622,7 +552,6 @@ md"""
 # ╠═3986a24a-689c-48b8-8e78-ec5d3842d0d7
 # ╠═938d8677-7898-43c8-b07c-6e9d7f3c7e2e
 # ╠═c5888be4-c641-4871-81a5-11d01ace12ed
-# ╠═127069e6-6da1-4742-88b4-a0d398a88b24
 # ╠═987efbb5-8a48-4c83-9a20-50e5fbd0a16d
 # ╠═939e3a45-03cd-4e07-a672-10c32cf2637e
 # ╠═7100eeed-5604-4d81-855e-b08cd40135a6
@@ -646,35 +575,6 @@ md"""
 # ╠═c3f16558-43b8-4822-98af-daf8e06cd1df
 # ╠═834739e9-fa7f-43f0-beac-ae5790575255
 # ╠═28316a84-710b-443e-a136-43fcfd821afa
-# ╠═d298a226-f5df-4636-9b41-2a302858592f
-# ╠═3e34982e-f791-4d96-9dde-27ee29720114
-# ╠═d89e4c44-6335-40ec-a158-ce1940a57cd5
-# ╠═47901238-c0ff-45f3-a0c7-f0bd60f10162
-# ╠═f89db6b4-d805-4a1e-b9e6-43542c29e204
-# ╠═d3a24890-989e-4832-a98a-ce60e5b5bdf0
-# ╠═bc36251b-b48d-42af-8c74-ae70744a0838
-# ╠═8196a315-eeb8-457b-8f2c-5b4591521f5f
-# ╠═b4fd1811-ee6d-4f8d-87d7-2f13c328060a
-# ╠═eed9dc74-6b7c-4fd1-b6e0-0f28a213ff26
-# ╠═fc787595-9163-410f-82f4-2252f5ebaf63
-# ╠═be54942b-0bec-49cf-9687-0a70143a63bd
-# ╠═60a07e1e-f85d-45af-908b-b9ae3adc6220
-# ╠═0e3395a1-4137-4264-8309-49c0c7e66e42
-# ╠═7cd1fb34-8f79-4d0d-801b-c2b6883085f2
-# ╠═086173f9-a16b-4201-9b1e-439ae9e08dc8
-# ╠═f2c3a527-bc28-4f58-aedb-74101c383293
-# ╠═7479fd7b-c81c-475a-9dbb-fdaea5ab8005
-# ╠═2d2882e1-5980-45a7-9891-9b328de9fa3d
-# ╠═c1e71e46-7b55-424b-b242-5609ddbd9a10
-# ╠═f8f6bf15-f5a6-4738-8bbd-d7adc9a214ed
-# ╠═b142fbc0-baca-4205-83d5-ea85c55baf7c
-# ╠═b9727123-582b-4f28-a073-c004e97d8b24
-# ╠═1d3d3ed9-4127-4fcf-822a-43f38c5632c7
-# ╠═1d94af98-b90b-4a57-af2d-eaf0aefa8fbd
-# ╠═37f7be98-67f0-43e2-b8b1-e2d48f6f8e53
-# ╠═83470d02-8fcd-45d5-acda-3f28da0c547b
-# ╠═dc712259-89aa-4138-a7b9-042956c89a33
-# ╠═c15d42ef-3b56-4093-b84a-0df13e46132f
 # ╠═fa011932-61e2-4377-bbf9-eb0bcd8c1b22
 # ╠═1783d8de-90df-434d-b71d-43c39a0d3579
 # ╠═fcba3e01-9464-4f95-8144-d8d1671b265e
@@ -686,7 +586,9 @@ md"""
 # ╠═1f92dabf-8597-4ef8-9e05-628e922defbf
 # ╠═6efdb3ef-4f6e-4073-bfa1-501732088e5c
 # ╠═097f05ad-8723-4693-bfba-c350cd478f54
-# ╠═417fa98b-38d1-434a-a44c-2948d71bec42
+# ╠═a0d12beb-a1e8-4f32-902d-233308f0a783
 # ╠═28fbe11a-4132-41e1-8ea8-3b65749f41b1
 # ╠═8d14ae41-a062-4d89-8e5a-e78b62cc0564
 # ╠═67c99b55-6874-4b3f-adea-caf12504bf46
+# ╠═127069e6-6da1-4742-88b4-a0d398a88b24
+# ╠═417fa98b-38d1-434a-a44c-2948d71bec42

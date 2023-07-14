0c1dea0878b158412d67553e2dc209a0ab461802
Jan Leppert <68964801+JanLeppert@users.noreply.github.com>
2023-07-14 18:51:42 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,34 @@
-using Test, GasChromatographySystems
+using Test, CSV, DataFrames, GasChromatographySystems
 
-@testset "example systems" begin
+#@testset "example systems" begin
     # define some example systems
-    ex_series = GasChromatographySystems.example_SeriesSystem()
+    ex_series = GasChromatographySystems.SeriesSystem(sps = ["SLB5ms", "SPB50", "Wax", "Wax"])
     @test ex_series.modules[1].length == 10.0 
-    ex_split = GasChromatographySystems.example_SplitSystem()
+    ex_split = GasChromatographySystems.SplitSystem(sps = ["SLB5ms", "SPB50", "Wax"])
     @test GasChromatographySystems.ne(ex_split.g) == 3
-    ex_GCxGC_TM = GasChromatographySystems.example_GCxGC_TM_simp()
-    @test isnan(ex_GCxGC_TM.pressurepoints[2].pressure_steps[1])
-    ex_GCxGC_FM = GasChromatographySystems.example_GCxGC_FM_simp()
-    @test ex_GCxGC_FM.options.mobile_phase == "He"
-end
+    #ex_GCxGC_TM_simp = GasChromatographySystems.GCxGC_TM_simp(sp1 = "SLB5ms", sp2 = "Wax")
+    #@test isnan(ex_GCxGC_TM_simp.pressurepoints[2].pressure_steps[1])
+    #ex_GCxGC_FM_simp = GasChromatographySystems.GCxGC_FM_simp(sp1 = "SLB5ms", sp2 = "Wax")
+    #@test ex_GCxGC_FM_simp.options.mobile_phase == "He"
+    ex_GCxGC_TM = GasChromatographySystems.GCxGC_TM(sp1 = "SLB5ms", sp2 = "Wax", spTL = "Wax", spM = "Wax")
+    @test ex_GCxGC_TM.options.mobile_phase == "He"
+    # run simulations on these systems
+    # data for soultes
+    db_file = string(@__DIR__, "/data/Database_test.csv")
+    db_dataframe = DataFrame(CSV.File(db_file, header=1, silencewarnings=true))
+	db_dataframe[!, :No] = collect(1:length(db_dataframe.Name))
+    selected_solutes = ["5-Nonanol", "Undecane", "2-Nonanol"]
+    # graph to parameters
+    par_series = GasChromatographySystems.graph_to_parameters(ex_series, db_dataframe, selected_solutes)
+    @test par_series[1].col.sp == ex_series.modules[1].stationary_phase
+    par_split = GasChromatographySystems.graph_to_parameters(ex_split, db_dataframe, selected_solutes)
+    @test par_split[2].col.sp == ex_split.modules[2].stationary_phase  
+    #par_GCxGC_TM_simp = GasChromatographySystems.graph_to_parameters(ex_GCxGC_TM_simp, db_dataframe, selected_solutes)
+    #@test par_GCxGC_TM_simp[1].col.sp == ex_GCxGC_TM_simp.modules[1].stationary_phase 
+    #par_GCxGC_FM_simp = GasChromatographySystems.graph_to_parameters(ex_GCxGC_FM_simp, db_dataframe, selected_solutes) 
+    #@test par_GCxGC_FM_simp[1].col.sp == ex_GCxGC_FM_simp.modules[1].stationary_phase
+    par_GCxGC_TM = GasChromatographySystems.graph_to_parameters(ex_GCxGC_TM, db_dataframe, selected_solutes)
+    @test par_GCxGC_TM[4].col.sp == ex_GCxGC_TM.modules[4].stationary_phase 
+#end
 
 println("Test run successful.")
